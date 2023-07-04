c8e62bd83f5aa4be6fad92b2a2a5aed2514528bf
rana <minami.smd@gmail.com>
2023-07-04 19:02:32 +0900

    rewrite scripts

--- download_stamp.sh
+++ download_stamp.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-if (( $# < 1 )); then
-    echo 'Usage: dlstamp PRODUCT_ID' >&2
-    exit 1
-fi
+main () {
+    (( $# < 1 )) && {
+        echo "Usage: $(basename $0) PRODUCT_ID" >&2
+        return 1
+    }
 
-product_url="https://store.line.me/stickershop/product/$1/ja"
-img_html=$(curl -sS "$product_url" | grep '"mdCMN09Image"') || {
-    echo 'Not found' >&2
-    exit 1
+    local product_url img_html os stamp_url
+
+    product_url="https://store.line.me/stickershop/product/$1/ja"
+    img_html=$(curl -s "$product_url" | grep '"mdCMN09Image"') || {
+        echo 'Not found' >&2
+        return 1
+    }
+    os=$(echo "$img_html" | head -n 1 | grep -Po '\d{5}/\K\w+')
+
+    case "$os" in
+        'android')
+            png='sticker.png' ;;
+        'iPhone')
+            png='sticker@2x.png' ;;
+        *)
+            return 1 ;;
+    esac
+
+    while read -r id
+    do
+        stamp_url="https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/${os}/${png}"
+        echo "$stamp_url"
+        curl -sS "$stamp_url" -o "${id}.png"
+    done < <(echo "$img_html" | grep -Po '\d{5,}')
 }
-os=$(echo "$img_html" | head -n 1 | grep -Po '\d{5}/\K\w+')
 
-case "$os" in
-    'android')
-        png='sticker.png' ;;
-    'iPhone')
-        png='sticker@2x.png' ;;
-    *)
-        exit 1 ;;
-esac
-
-while read -r id
-do
-    stamp_url="https://stickershop.line-scdn.net/stickershop/v1/sticker/${id}/${os}/${png}"
-    echo "$stamp_url"
-    curl -sS "$stamp_url" -o "${id}.png"
-done < <(echo "$img_html" | grep -Po '\d{5,}')
+main "$@"
--- passwd_gen.sh
+++ passwd_gen.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-len=10
-(( $# > 0 )) && len=$1
-base_str=$(cat /dev/urandom | base64 | head | paste -sd '')
-echo ${base_str:0:$len}
+main () {
+    local lower=false upper=false
+
+    while getopts 'hlu' OPT
+    do
+        case $OPT in
+            h ) echo "Usage: $(basename $0) [-hlu] [length]"; return 0 ;;
+            l ) lower=true ;;
+            u ) upper=true ;;
+            * ) return 1 ;;
+        esac
+    done
+
+    shift $(( OPTIND - 1 ))
+
+    local -i len=$1;
+    (( len == 0 && (len = 10) ))
+
+    local rand_str=$(base64 /dev/urandom | head | paste -sd '')
+    local pw=${rand_str:0:$len}
+
+    $lower && pw=${pw,,}
+    $upper && pw=${pw^^}
+
+    echo $pw
+}
+
+main "$@"
