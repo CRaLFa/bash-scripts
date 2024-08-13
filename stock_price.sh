e021176bd557b4b44fe5fcddd6e00b171ba4dc5c
CRaLFa <minami.smd@gmail.com>
2024-08-13 13:54:41 +0900

    use GNU parallel

--- stock_price.sh
+++ stock_price.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+check_dependency () {
+    which pup &> /dev/null || {
+        echo 'pup is required' >&2
+        return 1
+    }
+    which parallel &> /dev/null || {
+        echo 'parallel is required' >&2
+        return 1
+    }
+    return 0
+}
+
 stock_price () {
-    local code html price
-    for code in "$@"
-    do
-        html=$(curl -s "https://finance.yahoo.co.jp/quote/${code}.T")
-        [ -z "$html" ] && continue
-        pup 'h2._6uDhA-ZV text{}' <<< "$html"
-        price=()
-        price+=($(pup 'span._1mwPgJ2S span._3rXWJKZF text{}' <<< "$html"))
-        price+=($(pup 'span._30Ey7Bcp > span.Y_utZE_b > span._3rXWJKZF text{}' <<< "$html"))
-        price+=("($(pup 'span._30Ey7Bcp > span._3BXIqAcg > span._3rXWJKZF text{}' <<< "$html") %)")
-        echo -e "${price[@]}\n"
-    done
+    local code="$1" html price
+    html=$(curl -s "https://finance.yahoo.co.jp/quote/${code}.T")
+    [ -z "$html" ] && return
+    pup 'h2._6uDhA-ZV text{}' <<< "$html"
+    price=()
+    price+=($(pup 'span._1mwPgJ2S span._3rXWJKZF text{}' <<< "$html"))
+    price+=($(pup 'span._30Ey7Bcp > span.Y_utZE_b > span._3rXWJKZF text{}' <<< "$html"))
+    price+=("($(pup 'span._30Ey7Bcp > span._3BXIqAcg > span._3rXWJKZF text{}' <<< "$html") %)")
+    echo -e "${price[@]}\n"
 }
 
 main () {
@@ -20,14 +29,15 @@ main () {
         echo "Usage: $(basename $0) [-w INTERVAL] STOCK_CODE..." >&2
         return
     }
+    check_dependency || return
     local cmdline=()
     [[ "$1" = '-w' && "$2" =~ ^[0-9.]+$ ]] && {
         local -i interval=$2
-        cmdline+=(watch -n $interval)
-        export -f stock_price
+        cmdline+=("watch -n $interval")
         shift 2
     }
-    cmdline+=(stock_price "$@")
+    cmdline+=("parallel -j 0 -k stock_price ::: $@")
+    export -f stock_price
     eval ${cmdline[@]}
 }
 
