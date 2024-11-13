075277ad94b954086d8f031b3669a4e91b1c8f8a
CRaLFa <minami.smd@gmail.com>
2024-11-13 10:39:40 +0900

    refactor

--- xterm_256_colors.sh
+++ xterm_256_colors.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 fg_color () {
-	local -i code="$1"
-	printf '\e[38;5;%dm\e[40m %3d' "$code" "$code"
+	printf '\e[38;5;%dm\e[40m %3d' "$1" "$1"
 }
 
 bg_color () {
-	local -i code="$1"
-	printf '\e[37m\e[48;5;%dm %3d' "$code" "$code"
+	printf '\e[37m\e[48;5;%dm %3d' "$1" "$1"
 }
 
 new_line () {
@@ -15,15 +13,14 @@ new_line () {
 }
 
 main () {
+	local -i c
 	for c in {0..255}
 	do
 		(( (c <= 16 && c % 8 == 0) || (c > 16 && c % 6 == 4) )) && new_line
 		(( c == 16 )) && new_line
-
 		fg_color "$c"
 		bg_color "$c"
 	done
-
 	new_line
 }
 
