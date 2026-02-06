197900297d39bc3117aff81698f088b3cdb1a021
CRaLFa <minami.smd@gmail.com>
2026-02-06 18:10:45 +0900

    refactor

--- cal_with_holiday.sh
+++ cal_with_holiday.sh
@@ -3,25 +3,25 @@
 set -euo pipefail
 
 main () {
-	local y ym_from ym_to holidays ym
+	local y from to holidays ym
 
 	y=$(date '+%Y')
-	ym_from=$(( y * 100 + $(date '+%m') ))
-	ym_to=$ym_from
+	from=$(( y * 100 + $(date '+%m') ))
+	to=$from
 
 	if [[ "$*" == *3* ]]; then
-		ym_from=$(( $(date -d '-1 month' '+%Y') * 100 + $(date -d '-1 month' '+%m') ))
-		ym_to=$(( $(date -d '+1 month' '+%Y') * 100 + $(date -d '+1 month' '+%m') ))
+		from=$(( $(date -d '-1 month' '+%Y') * 100 + $(date -d '-1 month' '+%m') ))
+		to=$(( $(date -d '+1 month' '+%Y') * 100 + $(date -d '+1 month' '+%m') ))
 	elif [[ "$*" == *y* ]]; then
-		ym_from=$(( y * 100 + 1 ))
-		ym_to=$(( y * 100 + 12 ))
+		from=$(( y * 100 + 1 ))
+		to=$(( y * 100 + 12 ))
 	fi
 
 	holidays="$(curl -s 'https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv' | nkf -w -Lu | grep -E "$(( y - 1 ))|${y}|$(( y + 1 ))" | tr ',' '\t')"
 
 	cal "$@"
 
-	for (( ym = ym_from; ym <= ym_to; ym++ ))
+	for (( ym = from; ym <= to; ym++ ))
 	do
 		grep "$(( ym / 100 ))/$(( ym % 100 ))/" <<< "$holidays" || true
 	done
